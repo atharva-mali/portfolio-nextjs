b1cd29928634aa3a0a1b79aafa1cc09fa28ec66c
Atharva Mali <atharva.mali3081@gmail.com>
2022-09-10 11:42:26 +0530

    Added About Section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 
@@ -13,6 +14,8 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
+      <About />
+
 
     </div>
   )
