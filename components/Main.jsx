4e4ded29d3cb3bf2b605df436586931c78e1503c
Atharva Mali <atharva.mali3081@gmail.com>
2022-09-11 14:23:10 +0530

    Fixed apos error

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,16 +10,16 @@ const Main = () => {
       <div className="max-width-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET'S BUILD SOMETHING TOGETHER
+            LET&apos;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I'm<span className="text-[#5651e5]"> Atharva</span>
+            Hi, I&apos;m<span className="text-[#5651e5]"> Atharva</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            I'm a front-end web developer specializing in building (and
+            I&apos;m a front-end web developer specializing in building (and
             occasionally designing) exceptional digital experiences. Currently
-            I'm focused on building responsive front-end web applications while
+            I&apos;m focused on building responsive front-end web applications while
             learning back-end technologies.
           </p>
           <Link href="/#projects">
